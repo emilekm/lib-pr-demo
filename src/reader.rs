a39c9d1ec2c652b04a1957cdd00715c3f0474a97
cassius23 <emilekm@gmail.com>
2022-10-30 17:03:27 +0100

    Move to using demo container

--- src/reader.rs
+++ src/reader.rs
@@ -3,25 +3,74 @@ use std::{
     io::{Cursor, Read, Seek, SeekFrom},
 };
 
-use bincode::{de::read::Reader, error::DecodeError};
+use bincode::{config, de::read::Reader, error::DecodeError};
 use compress::zlib;
 
+use crate::{
+    messages,
+    types::{self, Messages},
+};
+
 pub struct DemoReader {
+    config: config::Configuration<
+        config::LittleEndian,
+        config::Fixint,
+        config::SkipFixedArrayLength,
+        config::NoLimit,
+    >,
     cursor: Cursor<Vec<u8>>,
+    last_length: u16,
 }
 
 impl DemoReader {
-    pub fn from_file(file: File) -> Self {
+    pub fn new(file: File) -> Self {
         let mut decompressed = Vec::new();
         _ = zlib::Decoder::new(file).read_to_end(&mut decompressed);
 
+        let config = bincode::config::standard()
+            .with_little_endian()
+            .with_fixed_int_encoding()
+            .skip_fixed_array_length();
+
         Self {
             cursor: Cursor::new(decompressed),
+            config,
+            last_length: 0,
         }
     }
 
-    pub fn skip_message(&mut self, size: u16) -> u64 {
-        self.cursor.seek(SeekFrom::Current(size as i64)).unwrap()
+    pub fn skip_message(&mut self) -> u64 {
+        self.cursor
+            .seek(SeekFrom::Current(self.last_length as i64))
+            .unwrap()
+    }
+
+    pub fn has_message(&mut self) -> bool {
+        self.cursor.get_ref().len() as u64 - self.cursor.position() >= 3
+    }
+
+    pub fn read_message(&mut self) -> Result<Messages, DecodeError> {
+        let config = self.config;
+        let header: messages::Header = bincode::decode_from_reader(&mut self, config)?;
+        self.last_length = header.length;
+        match header.typ {
+            types::SERVER_DETAILS => Ok(Messages::ServerDetails(bincode::decode_from_reader(
+                &mut self, config,
+            )?)),
+            types::PLAYER_UPDATE => Ok(Messages::PlayerUpdate(bincode::decode_from_reader(
+                &mut self, config,
+            )?)),
+            types::PLAYER_ADD => Ok(Messages::PlayerAdd(bincode::decode_from_reader(
+                &mut self, config,
+            )?)),
+            types::PLAYER_REMOVE => Ok(Messages::PlayerRemove(bincode::decode_from_reader(
+                &mut self, config,
+            )?)),
+            types::VEHICLE_UPDATE => Ok(Messages::VehicleUpdate(bincode::decode_from_reader(
+                &mut self, config,
+            )?)),
+            _ => Ok(Messages::Invalid(header)),
+        }
     }
 }
 
--- src/types.rs
+++ src/types.rs
@@ -1,12 +1,23 @@
+use crate::messages;
+
+pub const SERVER_DETAILS: u8 = 0;
+
+pub const PLAYER_UPDATE: u8 = 16;
+pub const PLAYER_ADD: u8 = 17;
+pub const PLAYER_REMOVE: u8 = 18;
+
+pub const VEHICLE_UPDATE: u8 = 32;
+
 #[derive(Debug)]
-pub enum MessageType {
-    Invalid = -1,
+pub enum Messages {
+    Invalid(messages::Header),
 
-    ServerDetails = 0,
+    ServerDetails(messages::ServerDetails),
 
-    PlayerUpdate = 10,
-    PlayerAdd = 11,
-    PlayerRemove = 12,
+    PlayerUpdate(messages::PlayerUpdate),
+    PlayerAdd(messages::PlayerAdd),
+    PlayerRemove(messages::PlayerRemove),
 
-    VehicleUpdate = 20,
+    VehicleUpdate(messages::VehicleUpdate),
 }
+
